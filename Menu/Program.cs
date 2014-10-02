00da3432b1b10f99767d88ee451dc78aa1229d5f
Learning <carlhen@users.noreply.github.com>
2014-10-02 20:22:23 +0200

    Ferdig med de tre funksjonnene i menyen

--- Menu/Program.cs
+++ Menu/Program.cs
@@ -29,12 +29,66 @@ namespace Menu
             Console.WriteLine("Tast 3 for: Meter til fot");
             //mer ?
             choice = Console.ReadLine();//users choice
-                       
+            if(choice == "1")
+            {
+                double kg;
+                Console.WriteLine("Tast in antall kg");
+                kg = double.Parse(Console.ReadLine());
+                double pounds = prog.ConvertKgToPounds(kg);
+                Console.WriteLine(kg + " kg" +" = " + pounds + " pounds");
+                Console.ReadLine();
+            }
+            if(choice == "2")
+            {
+                double c;
+                Console.WriteLine("Tast in antall celcius");
+                c = double.Parse(Console.ReadLine());
+                double f = prog.CelsiusToFahrenheit(c);
+                Console.WriteLine(c + " °C" + " = " + f + " °F");
+                Console.ReadLine();
+            }
+            if (choice == "3")
+            {
+                double meter;
+                Console.WriteLine("Tast in antall meter");
+                meter = double.Parse(Console.ReadLine());
+                double feet = prog.CelsiusToFahrenheit(meter);
+                Console.WriteLine(meter + " meter" + " = " + feet + " fot");
+                Console.ReadLine();
+            }       
         }
 
         private double ConvertKgToPounds(double kg)//this time we are using decimals. Mind the datatype. it is "double". 1 kilogram = 2.20462262 pounds
         {
             return kg * 2.20462262;
+        }
+        private double CelsiusToFahrenheit(double c)
+        {
+            return c * 9/5 + 32;
+        }
+        private double MeterToFeet(double meter)
+        {
+            return meter * 3.2808399;
+        }
+        /// <summary>
+        /// Ber om et helltall og vill loope hvis det får et tall med decimal
+        /// </summary>
+        /// <param name="text">Hva den skal spørre om</param>
+        /// <returns>Returnerer tallet</returns>
+        int NumberInputWithoutDecimal(string text)
+        {
+            bool numberCheck;
+            int number;
+            while (true)
+            {
+                Console.WriteLine(text);
+                numberCheck = int.TryParse(Console.ReadLine(), out number);
+                if (numberCheck == true)
+                {
+                    return number;
+                    break;
+                }
+            }
         }
     }
 }
